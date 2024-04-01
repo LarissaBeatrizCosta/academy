3fc0009f1bb6630c455e4d60d4b44daeb8bb3b9a
Lariss Beatriz Costa <larissa.beatrizzcosta@gmail.com>
2024-04-01 18:21:07 -0300

    finished d1

---
+++ dd1/d1.dart
@@ -0,0 +1,22 @@
+import 'dart:math';
+
+double CalcularArea(raio) {
+  var result1= pi * pow(raio, 2);
+  return result1;
+}
+
+double CalcularPerimetro(raio) {
+  var result2= 2 * pi * raio;
+  return result2;
+}
+
+void main() {
+  final raios = [5, 8, 12, 7.3, 18, 2, 25];
+
+  for (var raio in raios) {
+        print("Raio $raio, area: ${CalcularArea(raio).toStringAsFixed(2)}, "
+            "perimetro: ${CalcularPerimetro(raio).toStringAsFixed(2)}");
+  }
+}
+
+// Tempo Gasto: 08:25,04

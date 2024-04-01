d8a9243fe111459e51ae6e9912d796c7600ff783
Lariss Beatriz Costa <larissa.beatrizzcosta@gmail.com>
2024-04-01 19:48:10 -0300

    metade do d2

---
+++ dd1/d3.dart
@@ -0,0 +1,36 @@
+int contarTamanho(String texto) {
+  var result = texto.split("").length;
+  return result;
+}
+
+int contarFrases(String texto) {
+  var result = texto.split(".").length;
+  return result;
+}
+
+int contarVogais(String texto) {
+  var vogal = "AEIOU";
+  var result = 0;
+  for (var letras = 0; letras < texto.length; letras++) {
+    if (vogal.contains(texto[letras].toUpperCase())) {
+      result++;
+    }
+  }
+  return result;
+}
+
+List<String> contarConsoantes(String texto) {
+return [""];
+}
+
+void main() {
+  var texto = "Lorem ipsum dolor sit amet, "
+      "consectetur adipiscing elit. Nam venenatis nunc et posuere vehicula. "
+      "Mauris lobortis quam id lacinia porttitor.";
+
+  print("paragrafo: $texto \n Número de palavras:"
+      "Tamanho do texto:${contarTamanho(texto)} \n "
+      "Numero de frases: ${contarFrases(texto)} \n"
+      "Numero de vogais: ${contarVogais(texto)} \n"
+      "Numero de consoantes: ${contarConsoantes(texto)}");
+}

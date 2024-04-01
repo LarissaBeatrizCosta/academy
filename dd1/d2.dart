096765cedda8ab67ef198de7e765d652c48ea5fc
Lariss Beatriz Costa <larissa.beatrizzcosta@gmail.com>
2024-04-01 18:33:39 -0300

    finished d2

---
+++ dd1/d2.dart
@@ -0,0 +1,21 @@
+
+double TranformarF(double temperatura) {
+  var result1 = (temperatura * 1.8) + 32;
+  return result1;
+}
+
+double TranformarK(double temperatura) {
+  var result2 = temperatura + 273.15;
+  return result2;
+}
+
+void main() {
+  final temperaturas = [0.0, 4.2, 15.0, 18.1, 21.7, 32.0, 40.0, 41.0];
+
+  for (var temperatura in temperaturas) {
+    print(
+        "Celcius: $temperatura,fahrenheit: ${TranformarF(temperatura).toStringAsFixed(2)}, kelvin: ${TranformarK(temperatura).toStringAsFixed(2)}");
+  }
+}
+// Tempo Gasto: 03:52,43
+

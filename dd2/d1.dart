3ed60a0e352262e8ba0d4139b0c25b00e2944784
Lariss Beatriz Costa <larissa.beatrizzcosta@gmail.com>
2024-04-02 19:28:34 -0300

    terminei d1 dd2

---
+++ dd2/d1.dart
@@ -0,0 +1,50 @@
+import 'dart:math' as math;
+
+void main() {
+  final meioDeComunicacao = aleatorio();
+  meioDeComunicacao.fazerLigacao('(47) 99876-5432');
+}
+
+MeioDeComunicacao aleatorio() {
+  final meiosDeComunicacao = <MeioDeComunicacao>[
+    Telefone(),
+    Celular(),
+    Orelhao(),
+  ];
+
+  final random = math.Random();
+
+  return meiosDeComunicacao[random.nextInt(
+    meiosDeComunicacao.length,
+  )];
+}
+
+abstract class MeioDeComunicacao{
+  void fazerLigacao(String numero);
+}
+
+class Telefone extends MeioDeComunicacao{
+  @override
+  void fazerLigacao(String numero) {
+    print("[TELEFONE] Ligando para (47) 99876-5432...");
+  }
+}
+
+class Celular extends MeioDeComunicacao{
+  @override
+  void fazerLigacao(String numero) {
+    print("[CELULAR] Ligando para (47) 99876-5432...");
+  }
+}
+
+class Orelhao extends MeioDeComunicacao{
+  @override
+  void fazerLigacao(String numero) {
+    print("[ORELHÃO] Ligando para (47) 99876-5432...");
+  }
+}
+
+//Tempo Gasto: 13min
+
+
+

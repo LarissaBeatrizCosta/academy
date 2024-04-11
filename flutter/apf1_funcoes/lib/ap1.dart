a133dca33c095a7af00f06099267f4a5b7cd501f
Lariss Beatriz Costa <larissa.beatrizzcosta@gmail.com>
2024-04-11 18:32:15 -0300

    ap1 concluido

---
+++ flutter/apf1_funcoes/lib/ap1.dart
@@ -0,0 +1,49 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Color corDeFundo = Colors.white;
+
+  void sortearCor() {
+    setState(() {
+      corDeFundo = Color.fromRGBO(
+        Random().nextInt(256),
+        Random().nextInt(256),
+        Random().nextInt(256),
+        1,
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: corDeFundo,
+        appBar: AppBar(
+          title: Text('Exercício 1'),
+        ),
+        body: Center(
+          child: TextButton(
+            onPressed: () {
+              print("Sortear cor");
+              sortearCor();
+            },
+            child: Text('Meu Botão', style: TextStyle(color: Colors.black)),
+          ),
+        ),
+      ),
+    );
+  }
+}

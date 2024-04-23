a0b6f436e7faa3da920c78a6072d85fa6caa360d
Lariss Beatriz Costa <larissa.beatrizzcosta@gmail.com>
2024-04-23 18:08:53 -0300

    ap1 iniciado e desafio de jogoDaVelha

---
+++ dart/desafioLince/jogoDaVelha.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+void main() {
+  List<String> posicaoTabuleiro = EscolherPosicao();
+  JogarVelha(posicaoTabuleiro);
+}
+
+List<String> EscolherPosicao() {
+  print(("Escolha a posição de coluna de 1 a 3: "));
+  String posicaoColuna = stdin.readLineSync()!;
+  print(("Escolha a posição de linha de 1 a 3: "));
+  String posicaoLinha = stdin.readLineSync()!;
+
+  return [posicaoColuna, posicaoLinha];
+}
+
+void JogarVelha(List<String> posicaoEscolhida) {
+  int rodada = 0;
+  int posicaoColuna = int.parse(posicaoEscolhida[1]);
+  int posicaoLinha = int.parse(posicaoEscolhida[0]);
+
+  for (int i = 1; i < 4; i++) {
+    String linha = " |";
+    for (int j = 1; j < 4; j++) {
+      if (posicaoColuna == i && posicaoLinha == j) {
+        if (rodada % 2 == 0) {
+          linha += "X|";
+        }else{
+          linha += "O|";
+        }
+      } else
+        linha += " |";
+    }
+    print(linha);
+  }
+  rodada++;
+}
---
+++ flutter/apf6_funcoes/android/app/src/main/kotlin/com/example/apf6_funcoes/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.apf6_funcoes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()

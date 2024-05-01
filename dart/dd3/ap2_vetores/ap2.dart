03fba6cea7be576aceae6552636365a823a8ff29
Lariss Beatriz Costa <larissa.beatrizzcosta@gmail.com>
2024-05-01 19:18:21 -0300

    ap2 tópico 2 do dd3 concluido

---
+++ dart/dd3/ap2_vetores/ap2.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+import 'dart:math';
+
+import '../ap1_formulas/ap3.dart';
+
+void main() {
+  imprimirVetorDeValoresEscolhidos();
+}
+
+int pegarTamanhoDoVetor() {
+  late int tamanhoDoVetorInteiro;
+
+  print("Digite o valor do tamanho da lista: ");
+  String? respostaTamanhoVetor = stdin.readLineSync();
+
+  if (respostaTamanhoVetor != null && respostaTamanhoVetor.isNotEmpty) {
+    try {
+      tamanhoDoVetorInteiro = int.parse(respostaTamanhoVetor);
+    } catch (e) {
+      print("Insira um número");
+    }
+  } else {
+    print("Inválido");
+  }
+  return tamanhoDoVetorInteiro;
+}
+
+List<double> pegarValores() {
+  int contador = 0;
+  int quantidadeDeValores = pegarTamanhoDoVetor();
+  List<double> listaDeValoresEscolhidos = [];
+  if (quantidadeDeValores != null && quantidadeDeValores != 0) {
+    try {
+       while(contador < quantidadeDeValores){
+         print("Qual número você deseja inserir na lista: ");
+         String? respostaDoValor = stdin.readLineSync();
+         if ( respostaDoValor != null && respostaDoValor.isNotEmpty){
+           double respostaDoValorDouble = double.parse(respostaDoValor);
+           listaDeValoresEscolhidos.add(respostaDoValorDouble);
+         }else{
+           print("Você precisa digitar um valor válido");
+         }
+         contador++;
+       }
+    } catch (e) {
+      print("Inválido, tente outro tamanho");
+    }
+  } else {
+    print("Tamanho inválido");
+  }
+  return listaDeValoresEscolhidos;
+}
+
+void imprimirVetorDeValoresEscolhidos() {
+  List<double> vetorEscolhidos = pegarValores();
+  print("Seu vetor com tamanho ${vetorEscolhidos.length} ficou assim: ${vetorEscolhidos}");
+
+}

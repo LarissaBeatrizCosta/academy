28fae6b6d3d8d8e457b95f565396eeb07656f737
Lariss Beatriz Costa <larissa.beatrizzcosta@gmail.com>
2024-05-13 19:08:42 -0300

    Criado banco de dados e tabela

---
+++ exerciciosFixacaoFlutter/ap2_provider/lib/Controllers/DataBase.dart
@@ -0,0 +1,30 @@
+import "package:path/path.dart";
+import 'package:sqflite/sqflite.dart';
+
+Future<Database> getDatabase() async {
+  final path = join(
+    await getDatabasesPath(),
+    "tarefas.db",
+  );
+
+  return openDatabase(
+    path,
+    onCreate: (db, version) {
+      db.execute(TabelaTarefa.createTable);
+    },
+    version: 1,
+  );
+}
+
+class TabelaTarefa {
+  static const String createTable = """
+  CREATE TABLE $tableName(
+  $id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  $name TEXT NOT NULL
+  );
+  """;
+
+  static const String tableName = "tarefa";
+  static const String id = "id";
+  static const String name = "name";
+}
